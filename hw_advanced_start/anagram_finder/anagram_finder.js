18316c2076d77f7baf2f46f8c6640afe6e1a35b8
Alexander Seton <alexanderseton@protonmail.com>
2021-11-17 16:54:55 +0000

    Extension completed.

--- hw_advanced_start/anagram_finder/anagram_finder.js
+++ hw_advanced_start/anagram_finder/anagram_finder.js
@@ -1,9 +1,32 @@
 const AnagramFinder = function (word) {
-
-}
+    this.word = word;
+    console.log("USER WORD:", this.word);
+};
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
-}
+    let failed = false;
+    otherWords.forEach((otherWord) => {
+        if (otherWord === this.word) {
+            console.log("TRUE");
+            failed = true;
+        }
+    });
+    // CAN'T BREAK FROM A FOREACH LOOP !!!!!!!!!!
+    if (failed === true) {
+        const outputEmpty = [];
+        console.log("ANAGRAM EXIT OUTPU:", outputEmpty);
+        return outputEmpty;
+    }
+    this.word = this.word.toLowerCase().replace(" ", "").split("").sort();
+    const output = otherWords.filter((otherWord) => {
+        const otherWordLetters = otherWord.toLowerCase().replace(" ", "").split("").sort();
+        console.log("OTHER WORD:", otherWordLetters);
+        const wordLetters = this.word;
+        // return otherWordLetters === wordLetters;
+        return JSON.stringify(otherWordLetters) === JSON.stringify(wordLetters);
+    });
+    console.log("OUTPUT:", output);
+    return output;
+};
 
 module.exports = AnagramFinder;
--- hw_advanced_start/anagram_finder/anagram_finder_spec.js
+++ hw_advanced_start/anagram_finder/anagram_finder_spec.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const AnagramFinder = require('./anagram_finder.js');
 
 describe('AnagramFinder', function () {
+
   it('should be able to detect an anagram', function () {
     const anagramFinder = new AnagramFinder('act');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['cat', 'dog']), ['cat']);
@@ -36,4 +37,5 @@ describe('AnagramFinder', function () {
     const anagramFinder = new AnagramFinder('word');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['']), []);
   });
+
 });
